5f11f4bb6fc77c99d4c7bf4d691f5b6b97e167e7
ImmortalSt <immortal74521@gmail.com>
2022-12-15 16:14:22 +1000

    changes

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@
 int main(){
     random_generator* random = new random_generator();
 
-    int* mas = random->get_random_massive(1000);
+    int* mas = random->get_random_massive(9000);
 
     for (int i = 0; i < 1000; i++) printf("%d ", mas[i]);
     printf("\n");
-
+    printf("BLABLA");
     return 0;
 }
